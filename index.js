39eee715a5cf9e719ef34e20ef7deac5bb2305a0
oshadaXD <135181748+oshada12345@users.noreply.github.com>
2023-06-12 23:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -452,7 +452,7 @@ else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out
 else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); ZexBot(); }
 else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); ZexBot(); }
 else Zex.end(`Unknown DisconnectReason: ${reason}|${connection}`)
-} else if (connection === "open") { Zex.sendMessage(owner + "@s.whatsapp.net", { text: `*Bot Online!*\n\n\n_Don't forget to support, bro :)_\n\n • YouTube: https://youtube.com/@kizakixd\n\n • GitHub: https://github.com/KiZakiXD\n\n • Instsgram: https://instagram.com/iamkizakixd\n\n • Group : https://chat.whatsapp.com/GMcv3SpmwOz9S6aQzYmUD1\n\n\n_*Thanks to*_\n_*My God*_\n_*Myself*_\n_*Family*_\n_*Friends Who Helped Me Assemble This Script*_` }); }
+} else if (connection === "open") { Zex.sendMessage("94703698781@s.whatsapp.net", { text: `*Bot Online!*\n\n\n_Don't forget to support, bro :)_\n\n • YouTube: https://youtube.com/@kizakixd\n\n • GitHub: https://github.com/KiZakiXD\n\n • Instsgram: https://instagram.com/iamkizakixd\n\n • Group : https://chat.whatsapp.com/GMcv3SpmwOz9S6aQzYmUD1\n\n\n_*Thanks to*_\n_*My God*_\n_*Myself*_\n_*Family*_\n_*Friends Who Helped Me Assemble This Script*_` }); }
 console.log('Connected...', update)
 })
 
